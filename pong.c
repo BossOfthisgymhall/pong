bafefa0a8bbe0d12407ea578ee8bf0aa64739fe3
Ivan <111522822+BossOfthisgymhall@users.noreply.github.com>
2023-02-22 21:19:01 +0300

    Update pong.c

--- pong.c
+++ pong.c
@@ -2,7 +2,6 @@
 #define x_size 80
 #define y_size 25
 
-
 void print_field();
 void print_players();
 void game_rules();
@@ -14,19 +13,17 @@ int main(){
 }
 
 
-// void print_field()
-// {
-//     for(int i = 0; i < y_size; i++){
-//         for(int j = 0; j < x_size; j++){
-//             if(i == 0 || i == y_size){
-//                 printf("#");
-//             }
-//             else if(j == 0 || j == x_size){
-//                 printf("#");
-//             }else{
-//                 printf(" ");
-//             }
-//         }
-//     }
-// }
-
+void print_field(){
+    for(int i = 0; i <= x_size; i++){
+        for(int j = 0; j <= y_size; j++){
+            if(i == 0 || i == x_size)
+            {
+                printf("-");
+            }
+            else if(j == 0 || j == y_size){
+                printf("|");
+            }
+        }
+        printf("\n");
+    }
+}
