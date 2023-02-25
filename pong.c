2fcf44c0fe5f4111b6984bb052b7d1b15280ddb0
Van <111522822+BossOfthisgymhall@users.noreply.github.com>
2023-02-25 21:26:56 +0300

    uznat' problems

--- pong.c
+++ pong.c
@@ -3,47 +3,74 @@
 #define shir 80
 
 void print_field();
-void move_players();
+int move_players(char move);
 void game_rules();
-void print_players();
+void print_players(int i, int j, int player_1, int player_2);
+void game(int player_1, int player_2);
 
 int main(){
-    
-    print_field();
+    int player_1 = 13;
+    int player_2 = 13;
+    game(player_1, player_2);
     return 0;
 }
 
 
-void print_field(){
-    for(int i = 0; i <= visot; i++){
-        for(int j = 0; j <= shir; j++){
+void print_field(int player_1, int player_2){
+    int i, j;
+    
+    for(i = 0; i <= visot; i++){
+        for(j = 0; j <= shir; j++){
             if(i == 0 || i == visot)
             {
-                printf("#");
-            }
-            else if(j == 0 || j == shir){
                 printf("#");
             }
             else{
                 printf(" ");
             }
+            print_players(i, j, player_1, player_2);
         }
         printf("\n");
     }
+    char move;
+    move = getchar();
+    move_players(move);
+    player_1 = player_1 + move_players(move);
+    // move_players(player_1, player_2);
+    // char move;
+    // move = getchar();
+    // switch(move){
+    //     case 'a': printf("%d\n",player_1++); break;
+    //     case 'z': printf("%d\n",player_1--); break;
+    //     case 'k': printf("%d\n",player_2++); break;
+    //     case 'm': printf("%d\n",player_2--); break;
+    //     default: break;
+    // }
+    printf("%d\n%d\n", player_1, player_2);
 }
 
-void move_players(){             //добавить в main
-    char move;
-    int move_ball = 0;
-    scanf("%c", &move);
-    switch(move){
-        case "a": ball++; break;
-        case "z": ball--; break;
-        case "k": ball++; break;
-        case "m": ball--; break;
-        default: break;
+void print_players(int i, int j, int player_1, int player_2){
+    if((player_1 >= i && player_1 <= i) && j == 1)
+    {
+        printf("]");
+    }
+    else if((player_2 >= i && player_2 <= i) && j == shir - 1){
+        printf("[");
     }
 }
-//void print_players(){
-//    
-//}
+int move_players(char move){
+    switch(move){
+        case 'a': return 1; break;
+        case 'z': return -1; break;
+        default: return 0; break;
+    }
+}
+
+void game(int player_1, int player_2){
+    int end_game_flag = 1;
+    while (end_game_flag != 100)
+    {
+        print_field(player_1, player_2);
+        end_game_flag++;
+    }
+}
