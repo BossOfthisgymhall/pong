353ba3be8d80b2bfef0034c5c6f48ac89fa96b3b
Van <111522822+BossOfthisgymhall@users.noreply.github.com>
2023-02-26 10:03:53 +0300

    Update pong.c

--- pong.c
+++ pong.c
@@ -3,26 +3,31 @@
 #define shir 80
 
 void print_field();
-void move_players(int player_1, int player_2);
 void game_rules();
-void print_players(int i, int j, int player_1, int player_2);
-void game(int player_1, int player_2);
+void print_players();
+void game();
+void move_ball();
 
 int main(){
     int player_1 = 13;
     int player_2 = 13;
+    int x_ball = shir /2;
+    int y_ball = visot /2;
     game(player_1, player_2);
     return 0;
 }
 
 
-void print_field(int player_1, int player_2){
-    int i, j;
+void print_field(int player_1, int player_2, int *x_ball, int *y_ball){
+    int i, j;    
     for(i = 0; i <= visot; i++){
         for(j = 0; j <= shir; j++){
             if(i == 0 || i == visot)
             {
-                printf("#");
+                printf("-");
+            }
+            else if (j == x_ball && i == y_ball){
+                printf("*");
             }
             else{
                 printf(" ");
@@ -30,16 +35,6 @@ void print_field(int player_1, int player_2){
             print_players(i, j, player_1, player_2);
         }
         printf("\n");
-    }
-    // move_players(player_1, player_2);
-    char move;
-    move = getchar();
-    switch(move){
-        case 'a': printf("%d\n",player_1++); break;
-        case 'z': printf("%d\n",player_1--); break;
-        case 'k': printf("%d\n",player_2++); break;
-        case 'm': printf("%d\n",player_2--); break;
-        default: break;
     }
     printf("%d\n%d\n", player_1, player_2);
 }
@@ -53,25 +48,24 @@ void print_players(int i, int j, int player_1, int player_2){
         printf("[");
     }
 }
-// void move_players(int player_1, int player_2){
-//     char move;
-//     move = getchar();
-//     if(move == 'a'){
-//         player_1++;
-//     }
-//     switch(move){
-//         case 'a': return player_1++; break;
-//         case 'z': return player_1--; break;
-//         case 'k': return player_2++; break;
-//         case 'm': return player_2--; break;
-//         default: break;
-//     }
-// }
-void game(int player_1, int player_2){
+
+void game(int player_1, int player_2, int *x_ball, int *y_ball){
     int end_game_flag = 1;
     while (end_game_flag != 100)
     {
-        print_field(player_1, player_2);
-        end_game_flag++;
+        print_field(player_1, player_2, *x_ball, *y_ball);
+        char move;
+        scanf("%c", &move);
+        switch(move){
+            case 'a': player_1++; break;
+            case 'z': player_1--; break;
+            case 'k': player_2++; break;
+            case 'm': player_2--; break;
+            default: break;
+        }
+        end_game_flag++; // временное условие для проверки работы
     }
+}
+void move_ball(){
+    
 }
