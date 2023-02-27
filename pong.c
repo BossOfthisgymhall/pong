c68f05db6fcf1eaded593919291c710ac0faf5ea
Van <111522822+BossOfthisgymhall@users.noreply.github.com>
2023-02-27 16:55:42 +0300

    Update pong.c

--- pong.c
+++ pong.c
@@ -11,14 +11,13 @@ void move_ball();
 int main(){
     int player_1 = 13;
     int player_2 = 13;
-    int x_ball = shir /2;
-    int y_ball = visot /2;
+
     game(player_1, player_2);
     return 0;
 }
 
 
-void print_field(int player_1, int player_2, int *x_ball, int *y_ball){
+void print_field(int player_1, int player_2, int x_ball, int y_ball){
     int i, j;    
     for(i = 0; i <= visot; i++){
         for(j = 0; j <= shir; j++){
@@ -40,20 +39,24 @@ void print_field(int player_1, int player_2, int *x_ball, int *y_ball){
 }
 
 void print_players(int i, int j, int player_1, int player_2){
-    if((player_1 >= i && player_1 <= i) && j == 1)
+    if((((player_1 >= i && player_1 <= i)) || (player_1 + 1 >= i && player_1 + 1 <= i) || 
+    (player_1 - 1 >= i && player_1 - 1 <= i)) && j == 2)
     {
         printf("]");
     }
-    else if((player_2 >= i && player_2 <= i) && j == shir - 1){
+    else if((((player_2 >= i && player_2 <= i)) || (player_2 + 1 >= i && player_2 + 1 <= i) || 
+    (player_2 - 1 >= i && player_2 - 1 <= i)) && j == shir - 2){
         printf("[");
     }
 }
 
-void game(int player_1, int player_2, int *x_ball, int *y_ball){
+void game(int player_1, int player_2){
     int end_game_flag = 1;
+    int x_ball = shir / 2;
+    int y_ball = visot / 2;
     while (end_game_flag != 100)
     {
-        print_field(player_1, player_2, *x_ball, *y_ball);
+        print_field(player_1, player_2, x_ball, y_ball);
         char move;
         scanf("%c", &move);
         switch(move){
@@ -61,11 +64,10 @@ void game(int player_1, int player_2, int *x_ball, int *y_ball){
             case 'z': player_1--; break;
             case 'k': player_2++; break;
             case 'm': player_2--; break;
+
             default: break;
         }
+        move_ball(x_ball, y_ball);
         end_game_flag++; // временное условие для проверки работы
     }
-}
-void move_ball(){
-    
 }
