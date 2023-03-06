667cbb7d993eabe08ca7c705adbf3ac87b0b1559
Van <111522822+BossOfthisgymhall@users.noreply.github.com>
2023-03-06 15:37:03 +0300

    Почти закончил

--- pong.c
+++ pong.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#define visot 25
+#define visot 26
 #define shir 80
 
 void print_field();
-void game_rules();
 void print_players();
 void game();
 
@@ -16,11 +15,11 @@ int main(){
 }
 
 
-void print_field(int player_1, int player_2, int x_ball, int y_ball){
+void print_field(int player_1, int player_2, int x_ball, int y_ball, int score_player_1, int score_player_2){
     int i, j;    
     for(i = 0; i <= visot; i++){
         for(j = 0; j <= shir; j++){
-            if(i == 0 || i == visot) // рисую верхнюю и нижнюю границу поля
+            if(i == 0 || i == visot - 1) // рисую верхнюю и нижнюю границу поля
             {
                 printf("#");
             }
@@ -37,6 +36,12 @@ void print_field(int player_1, int player_2, int x_ball, int y_ball){
             {
                 printf("[");
             }
+            else if(i == 26 && (j == 20)){
+                printf("%d", score_player_1);
+            }
+            else if(i == 26 && j == 60){
+                printf("%d", score_player_2);
+            }
             else{
                 printf(" ");
             }
@@ -52,28 +57,32 @@ void game(int player_1, int player_2){
     int flag = 0;
     int x_ball_prev = x_ball;
     int y_ball_prev = y_ball;  
-    while (end_game_flag)
+    int score_player_1 = 0;
+    int score_player_2 = 0;
+    while (end_game_flag) // дописать нормальные условия для завершения игры
     {
-        print_field(player_1, player_2, x_ball, y_ball);
+        print_field(player_1, player_2, x_ball, y_ball, score_player_1, score_player_2);
         char move;
         scanf("%c", &move);
-        switch(move){
-            case 'a': player_1--; break;
-            case 'z': player_1++; break;
-            case 'k': player_2--; break;
-            case 'm': player_2++; break;
-            case 'e': end_game_flag = 0; break;
-            default: break;
+        if((player_1 > 2 && player_1 < 24) || (player_2 > 2 && player_2 < 24)) // проблема с краем поля, нужно переделать
+        {
+            switch(move){
+                case 'a': player_1--; break;
+                case 'z': player_1++; break;
+                case 'k': player_2--; break;
+                case 'm': player_2++; break;
+                case 'e': end_game_flag = 0; break;
+                default: break;
+            }
         }
-        if((x_ball <= shir && x_ball >= 0) && ((y_ball == visot - 1) && (x_ball - 1 == x_ball_prev))){ //условия для отскока от пола слева направо | 
+        if((x_ball <= shir && x_ball >= 0) && ((y_ball == visot - 2) && (x_ball - 1 == x_ball_prev))){ //условия для отскока от пола слева направо | 
            flag = 1;
         }
-        if((x_ball <= shir && x_ball >= 0) && ((y_ball == visot - 1) && (x_ball + 1 == x_ball_prev))){  //условия для отскока от пола справа налево | 
+        if((x_ball <= shir && x_ball >= 0) && ((y_ball == visot - 2) && (x_ball + 1 == x_ball_prev))){  //условия для отскока от пола справа налево | 
            flag = 3;
         }
-        else if((x_ball == (shir - 1) && ((y_ball == player_2 || y_ball == player_2 + 1 || y_ball == player_2 - 1)) && (y_ball_prev == y_ball - 1))){  //условия для отскока от игрока 2 сверху вниз | добавить условия для игроков, пока отталкивается от стены
+        else if((x_ball == (shir - 1) && ((y_ball == player_2 || y_ball == player_2 + 1 || y_ball == player_2 - 1)) && (y_ball_prev == y_ball - 1))){  //условия для отскока от игрока 2 сверху вниз |
             flag = 2;
-            printf("voshel\n");
         }
         else if(((x_ball == (shir - 1) && (y_ball == player_2 || y_ball == player_2 + 1 || y_ball == player_2 - 1)) && (y_ball_prev == y_ball + 1))){ //условия для отскока от игрока 2 снизу вверх | 
             flag = 3;
@@ -86,13 +95,20 @@ void game(int player_1, int player_2){
         }
         else if((x_ball == 1) && ((y_ball == player_1 || y_ball == player_1 + 1 || y_ball == player_1 - 1) && (y_ball_prev == y_ball - 1))){ //условия для отскока от игрока 1 сверху вниз |
             flag = 1;
-            printf("voshel\n");
         }
         else if(((x_ball == 1) && ((y_ball == player_1 || y_ball == player_1 + 1 || y_ball == player_1 - 1)) && (y_ball_prev == y_ball + 1))){ //условия для отскока от игрока 1 снизу вверх |
             flag = 1;
         }
+        else if(x_ball > shir){
+            flag = 4;
+            score_player_1++;
+        }
+        else if(x_ball < 0)
+        {
+            flag = 4;
+            score_player_2++;
+        }
         printf("%d %d\n%d %d\n", x_ball, y_ball, x_ball_prev, y_ball_prev);
-        //printf("%d\n%d\n%d\n", x_ball, y_ball, flag);
         x_ball_prev = x_ball;
         y_ball_prev = y_ball;
         if(flag == 0) // вправо вниз
@@ -112,19 +128,10 @@ void game(int player_1, int player_2){
             x_ball--;
             y_ball--;
         }
+        else if(flag == 4){
+            x_ball = shir / 2;
+            y_ball = visot / 2;
+            flag = 0;
+        }
     }
 }
-
-// int score(int x_ball);
-// {
-//     int score_player_1 = 0;
-//     int score_player_2 = 0;
-//     if(x_ball > 80)
-//     {
-//         return score_player_2++;
-//     }
-//     else{
-//         return score_player_1++;
-//     }
-//     return 0;
-// }
